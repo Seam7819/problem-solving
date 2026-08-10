d5b114a4292bca960d73b782e1475e9823977c5e
Snu Seam <siamahme766@gmail.com>
2026-08-10 23:05:20 +0600

    f

--- ques1&2.js
+++ ques1&2.js
@@ -1,4 +1,4 @@
-// Q1. What is the difference between var, let, and const in JavaScript?
-"var is function-scoped and gets hoisted with a default value of undefined, so it can lead to bugs like variables leaking outside blocks. let and const are block-scoped, introduced in ES6 to fix that. The difference between them is that let allows reassignment, while const doesn't — though if it holds an object or array, the contents can still be mutated. In practice, I default to const, use let only when I know a value will change, and avoid var altogether."
+// "// Q1. What is the difference between var, let, and const in JavaScript?
+// "var is function-scoped and gets hoisted with a default value of undefined, so it can lead to bugs like variables leaking outside blocks. let and const are block-scoped, introduced in ES6 to fix that. The difference between them is that let allows reassignment, while const doesn't — though if it holds an object or array, the contents can still be mutated. In practice, I default to const, use let only when I know a value will change, and avoid var altogether."
 
-
+//  array, the contents can still be mutated. In practice, I default to const, use let only when I kno"
